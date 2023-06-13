ff7ab761645dfadc80e2812335966e7a31bdec8d
Marc <marc.weber54@gmail.com>
2023-06-12 22:50:19 -0400

    Add podman server running

--- src/controllers/podman.js
+++ src/controllers/podman.js
@@ -10,14 +10,19 @@ const allowedImages = [
 ];
 
 // Uses MEMORY_LIMIT (e.g. 256m - https://docs.podman.io/en/latest/markdown/podman-run.1.html#memory-m-number-unit) and TIME_LIMIT (in ms)
-async function run_podman(code, image){
+async function run_podman(code, image, pod_name = 'new'){
     // Restrict the image to the allowed ones to avoid DoS and prevent injection
     if(!allowedImages.includes(image)){
         throw new Error('Invalid image. Allowed images: ' + allowedImages.join(', '));
     }
 
+    // Create a random pod name if it's new
+    if(pod_name === 'new'){
+        pod_name = 'new:' + Math.random().toString(36).substring(7);
+    }
+
     // Spawn the podman image with a piped stdin, stdout, and stderr
-    const podman = spawn('podman', ['run', '-m', process.env.MEMORY_LIMIT, '-i', image], {
+    const podman = spawn('podman', ['run', '--pod=' + pod_name, '-m', process.env.MEMORY_LIMIT, '-i', image], {
         stdio: 'pipe'
     });
 
@@ -51,6 +56,29 @@ async function run_podman(code, image){
     ]);
 
     return [stdout, stderr, exit_code];
+}
+
+function run_server_podman(code, image, pod_name = 'new'){
+    // Restrict the image to the allowed ones to avoid DoS and prevent injection
+    if(!allowedImages.includes(image)){
+        throw new Error('Invalid image. Allowed images: ' + allowedImages.join(', '));
+    }
+
+    // Create a random pod name if it's new
+    if(pod_name === 'new'){
+        pod_name = 'new:' + Math.random().toString(36).substring(7);
+    }
+
+    // Spawn the podman image with a piped stdin, stdout, and stderr
+    const podman = spawn('podman', ['run', '--pod=' + pod_name, '-m', process.env.MEMORY_LIMIT, '-i', image], {
+        stdio: 'pipe'
+    });
+
+    // Send the code to the child process's stdin
+    podman.stdin.write(code);
+    podman.stdin.end();
+
+    return {process: podman, pod_name: pod_name.replace('new:', '')}
 }
 
 
