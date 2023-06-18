cdcfdb3c8286ea8fce2b6adb0dfbd5de763d12a0
Marc <marc.weber54@gmail.com>
2023-06-18 00:21:14 -0400

    Add code extraction because openAI keeps explaining itself

--- src/controllers/openai.js
+++ src/controllers/openai.js
@@ -1,5 +1,18 @@
 import { Configuration, OpenAIApi} from 'openai';
+import SimpleMarkdown from 'simple-markdown';
 
+
+function extract_code(text){
+    var syntaxTree = SimpleMarkdown.defaultBlockParse(text);
+    console.log('AI response: ' + JSON.stringify(syntaxTree, 0, 2));
+    var codeBlocks = [];
+    syntaxTree.forEach(function(block){
+        if(block.type === 'codeBlock'){
+            codeBlocks.push(block.content);
+        }
+    });
+    return codeBlocks.join('\n');
+}
 
 // Uses OPENAI_MODEL (e.g. gpt-3.5-turbo), and OPENAI_KEY
 async function generate(messages){
@@ -12,8 +25,16 @@ async function generate(messages){
         model: process.env.OPENAI_MODEL,
         messages
     });
-    console.log({result: ai_response.data.choices[0].message.content});
-    return ai_response.data.choices[0].message.content;
+
+    var generated_code = ai_response.data.choices[0].message.content;
+
+    // If a ``` is in the generated code, strip everything outside the code blocks
+    if(generated_code.includes("```")){
+        generated_code = extract_code(generated_code);
+    }
+
+    console.log(generated_code);
+    return generated_code
 }
 
 export { generate };
