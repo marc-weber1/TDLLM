ed4c59e3024aa7be97fc91e8be6fee2360768745
tetra <me@tetra.cool>
2023-06-28 15:22:11 -0600

    Replace codemirror with monaco-editor

--- tdllm-client/src/pages/Run.svelte
+++ tdllm-client/src/pages/Run.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-    import CodeMirror from "svelte-codemirror-editor";
-    import { javascript } from "@codemirror/lang-javascript";
-    import { run } from "$lib/TDLLMBackend";
-    import { code_runtab, stdout_runtab, stderr_runtab, exit_code_runtab } from "$lib/stores";
+  // import CodeMirror from "svelte-codemirror-editor";
+  // import { javascript } from "@codemirror/lang-javascript";
+  import { run } from "$lib/TDLLMBackend";
+  import MonacoEditor from "../components/MonacoEditor.svelte";
+  import { code_runtab, stdout_runtab, stderr_runtab, exit_code_runtab } from "$lib/stores";
 </script>
 
-<CodeMirror bind:value={$code_runtab} lang={javascript()} />
-<button on:click={async () => {
+<!-- <CodeMirror bind:value={$code_runtab} lang={javascript()} /> -->
+<MonacoEditor bind:code={$code_runtab} />
+<button
+  on:click={async () => {
     var resp = await run($code_runtab);
     stdout_runtab.set(resp.stdout);
     stderr_runtab.set(resp.stderr);
     exit_code_runtab.set(resp.exit_code);
-}}>Run</button>
+  }}>Run</button>
 <br />
-<textarea bind:value={$stdout_runtab} readonly/>
-<textarea bind:value={$stderr_runtab} readonly/>
+<textarea bind:value={$stdout_runtab} readonly />
+<textarea bind:value={$stderr_runtab} readonly />
 Exit code: {$exit_code_runtab}
--- tdllm-client/src/routes/+page.svelte
+++ tdllm-client/src/routes/+page.svelte
@@ -1,23 +1,14 @@
 <script>
-	import Tab1 from "../pages/Run.svelte";
-	import Tab2 from "../pages/CLI.svelte";
-	import Tab3 from "../pages/API.svelte";
+  import Tab1 from "../pages/Run.svelte";
+  import Tab2 from "../pages/CLI.svelte";
+  import Tab3 from "../pages/API.svelte";
   import Tabs from "../components/Tabs.svelte";
 
   // List of tab items with labels, values and assigned components
   let items = [
-    { label: "Run",
-		 value: 1,
-		 component: Tab1
-		},
-    { label: "CLI",
-		 value: 2,
-		 component: Tab2
-		},
-    { label: "API",
-		 value: 3,
-		 component: Tab3
-		}
+    { label: "Run", value: 1, component: Tab1 },
+    { label: "CLI", value: 2, component: Tab2 },
+    { label: "API", value: 3, component: Tab3 },
   ];
 </script>
 
