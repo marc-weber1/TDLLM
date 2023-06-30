70e366af3581983792ef30089cb2c720e380408d
Marc <marc.weber54@gmail.com>
2023-06-30 02:20:01 -0400

    Add Skeleton UI

---
+++ tdllm-client/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  // Your custom Skeleton theme:
+  import '../theme.postcss';
+
+  // This contains the bulk of Skeletons required styles:
+  import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+  // Finally, your application's global stylesheet (sometimes labeled 'app.css')
+  import '../app.postcss';
+</script>
+
+<slot />
